8f609e125d3da37b9287274d736343de9c760ae5
miyago9267 <miyagomizugen@gmail.com>
2022-05-10 13:13:58 +0800

    Repath & Rename

--- UVa/cpe_study/Solo/week3/458/sol.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-for s in sys.stdin:
-    for char in s:
-        print(chr(ord(char)-7),end='')
-    print()
--- UVa/cpe_study/Solo/week5/10071/sol.py
+++
@@ -1,6 +0,0 @@
-try:
-    while True:
-        v, t = map(int, input().split())
-        print(2*v*t)
-except EOFError:
-    pass
