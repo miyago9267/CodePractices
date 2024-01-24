52ed09e3cdc36242ca8ab9590b542dcc7c319493
miyago9267 <miyagomizugen@gmail.com>
2024-01-24 17:50:56 +0800

    feat: empty, init commit

---
+++ atcoder/abc337/B/sol.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define endll "\n\n"
+#define pb push_back
+#define IO ios_base::sync_with_stdio(0)
+#define ll long long
+#define inf 0x3f3f3f3f
+#define MAXN maxn
+#define MODN modn
+
+using namespace std;
+
+// ---------------------------------------------------------------- //
+
+class Solution{
+    public:
+        Solution(){}
+        ~Solution(){}
+    private:
+};
+
+void solve(){
+    string s, tmps; cin >> s;
+    tmps = s;
+    sort(s.begin(), s.end());
+
+    cout << (s == tmps ?
+        "Yes" : "No")
+        << endl;
+}
+
+signed main(){
+    IO;
+    #ifdef DEBUG
+        cin.tie(0);cout.sync_with_stdio(0);
+        string rootPath = "/data/miyago/ContestWriteUp";
+        freopen((rootPath+"/Debug/t.in").c_str(), "r", stdin);
+        freopen((rootPath+"/Debug/t.out").c_str(), "w", stdout);
+    #endif
+
+    solve();
+
+    return EXIT_SUCCESS;
+}
