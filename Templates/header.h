6956cd0c37f10a164db688a457615b1e11d2999b
miyago9267 <miyagomizugen@gmail.com>
2024-02-20 11:39:46 +0800

    feat: template defined and header

---
+++ Templates/header.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define endll "\n\n"
+#define pb push_back
+#define IO ios_base::sync_with_stdio(0)
+#define ll long long
+#define inf 0x3f3f3f3f
+#define MAXN maxn
+#define MODN modn
+
+#define DD(x) cout << #x << " = " << x << endl
+#define tt cout << "test" << endl
+#define fe(v) for(auto x : v) cout << x << " "; cout << endl
+#define pii pair<ll, ll>
+#define vi vector<ll>
+#define vvi vector<vi>
+#define cmp greater<ll>
+#define all(v) v.begin(), v.end()
+
+#define rootPath NEED_SETTING
+#define di (rootPath+"/Debug/t.in")
+#define do (rootPath+"/Debug/t.out")
